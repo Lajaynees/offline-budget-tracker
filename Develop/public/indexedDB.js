4b69a281df6fc776afefa1391cbfb73b4e979ac0
Lajaynees <lscingram@yahoo.com>
2021-10-15 22:45:57 -0400

    shorten onsuccess function-add object:onupgrade

--- Develop/public/indexedDB.js
+++ Develop/public/indexedDB.js
@@ -5,14 +5,27 @@ const request = indexedDB.open("budget", 1)
 
 //errors
 request.onerror = function(event) {
-    console.log("Error! " + event.target.errorCode);
+    console.log("TRY AGAIN! " + event.target.errorCode);
 };
 
 // connection ok
-request.onsuccess = function(event) {
-    db = event.target.result; 
+// request.onsuccess = function(event) {
+//     db = event.target.result; 
+//     if (navigator.onLine) {
+//         checkDatabase();
+//     }
+// };
+// connection ok-shortened
+request.onsuccess = ({ target }) => {
+    db = target.result;
     if (navigator.onLine) {
-        checkDatabase();
+      checkDatabase();
     }
 };
+        
+// Object -Upgrade
+request.onupgradeneeded = ({ target }) => {
+    let db = target.result;
+    db.createObjectStore("new_budget_sheet", { autoIncrement: true });
+  };
 
