ab5014762b6da67b608a5f7b4a71c4cfc667151d
Lajaynees <lscingram@yahoo.com>
2021-10-15 23:14:35 -0400

    collect new data function updated/getAll function executed

--- Develop/public/indexedDB.js
+++ Develop/public/indexedDB.js
@@ -29,3 +29,22 @@ request.onupgradeneeded = ({ target }) => {
     db.createObjectStore("new_budget_sheet", { autoIncrement: true });
   };
 
+// collecting new sheet data
+  function checkDatabase() {
+    const transaction = db.transaction(["new_budget_sheet"], "readwrite");
+    const store = transaction.objectStore("new_budget_sheet");
+
+// collect all records-run function when true
+    const getAll = store.getAll();
+    
+    getAll.onsuccess = function() {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+    
